404aca4257a1a5089601b349a9db2764f2b6470f
RamitVishwakarma <vishwakarmaramit@gmail.com>
2024-01-02 12:28:50 +0530

    fixed a bug

--- Recruitment2k24/src/App.jsx
+++ Recruitment2k24/src/App.jsx
@@ -4,7 +4,6 @@ import AdminLogin from "./components/admin/login";
 import Dashboard from "./components/admin/dashboard";
 import AllUsers from "./components/admin/allusers";
 import RegistrationForm from "./components/RegistrationForm";
-import AdminLogin from "./components/admin/login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -14,9 +13,9 @@ function App() {
         <Route exact path="/" element={<Homepage />} />
         <Route path="/domains" element={<Domains />} />
         <Route path="/register" element={<RegistrationForm />} />
-        <Route path="/admin-login" element={<AdminLogin />}/>
-        <Route path="/dashboard" element={<Dashboard />}/>
-        <Route path="/allusers" element={<AllUsers />}/>
+        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/allusers" element={<AllUsers />} />
       </Routes>
     </BrowserRouter>
   );
