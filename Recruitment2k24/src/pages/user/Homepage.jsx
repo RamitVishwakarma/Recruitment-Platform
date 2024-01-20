ef5dff04439568876c71809bf9dd534dc085b082
Aryan Kushwaha <aryankushwaha3101@gmail.com>
2024-01-20 17:09:19 +0530

    completed Homepage

--- Recruitment2k24/src/pages/user/Homepage.jsx
+++ Recruitment2k24/src/pages/user/Homepage.jsx
@@ -5,13 +5,22 @@ import Sapling from "../../../src/assets/sapling.svg";
 import GDSClogo from "../../../src/assets/gdsc_logo_centered.svg";
 import Homebg from "../../../src/assets/homepagebg.svg";
 import Recruitments from "../../../src/assets/recruitments-text.svg";
+import Timeline from "../../../src/assets/timeine.svg"
+import Pointer from "../../../src/assets/tri.svg"
+import ArrowRight from "../../../src/assets/arrow_circle_right.svg"
+
+import Android from "../../../src/assets/android.svg"
+import WebClub from "../../../src/assets/webclub.svg"
+import Programming from "../../../src/assets/programming.svg"
+import Design from "../../../src/assets/design.svg"
+import ML from "../../../src/assets/machinelearning.svg"
 
 import Lenis from '@studio-freight/lenis'
 
 export default function Homepage() {
   const lenis = new Lenis({
-    //larp: 0.5,
-    duration: 0.7,
+    larp: 0.1,
+    //duration: 0.7,
     easing: (t) => 1 - Math.pow(1 - t, 5),
   });
   function raf(time) {
@@ -24,11 +33,11 @@ export default function Homepage() {
   return (
     <>
       <Navbar />
-      <div className="flex flex-col gap-12 items-center mb-20 text-grey">
-        <img className="mt-60 mx-auto" src={Recruitments}></img>
+      <div className="flex flex-col gap-20 items-center mb-20 text-grey">
+        <img className="mt-52 mx-auto" src={Recruitments}></img>
 
         <div className="flex items-center gap-12">
-          <h2 className="font-bold text-4xl">Register Here</h2>
+          <h2 className="font-bold text-5xl">Register Here</h2>
           <button className="cta flex items-center gap-6 bg-lime px-10 rounded-xl py-4">
             <h5 className="text-2xl">Let&apos;s Go</h5>
             <svg xmlns="http://www.w3.org/2000/svg" width="33" height="24" viewBox="0 0 33 24" fill="none">
@@ -37,13 +46,13 @@ export default function Homepage() {
           </button>
         </div>
         {/* flame */}
-        <div className="mt-48 relative">
+        <div className="mt-36 text-center relative">
           <img src={Flame} alt="" />
           <p className="text-7xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 whitespace-nowrap">Every talent is precious to us</p>
         </div>
 
         {/* sapling */}
-        <div className="mt-48 mb-20 relative">
+        <div className="mt-48 text-center mb-4 relative">
           <img src={Sapling} alt="" />
           <p className="text-7xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 whitespace-nowrap">A place to hone your skills and grow</p>
         </div>
@@ -53,7 +62,7 @@ export default function Homepage() {
         </svg>
 
         {/* club intro */}
-        <div className="mt-20 flex flex-col gap-6 items-center mb-28">
+        <div className="mt-4 flex text-center flex-col gap-6 items-center">
           <h3 className="text-5xl">Welcome to</h3>
           <div className="relative">
             <img src={Homebg} className="w-screen" alt="" />
@@ -61,7 +70,95 @@ export default function Homepage() {
           </div>
           <h3 className="text-5xl">and find a place in our domains</h3>
         </div>
-      
+
+
+        {/* domains */}
+        <div className="cards">
+          <div className="row1">
+            <div className="card bg-purple/20">
+              <img src={Programming} alt="" />
+              <h2 className="text-purple">Programming</h2>
+              <p>The art of instructing computers through code to perform specific tasks, solve problems, and create software applications.</p>
+              <span className="text-purple">01</span>
+            </div>
+            <div className="card bg-red/20">
+              <img src={WebClub} alt="" />
+              <h2 className="text-red">Web Club</h2>
+              <p> The practice of creating and maintaining websites and web applications, encompassing both front-end and back-end development to ensure a seamless online user experience.</p>
+              <span className="text-red">02</span>
+            </div>
+            <div className="card bg-lime/20">
+              <img src={Android} alt="" />
+              <h2 className="text-lime">Android</h2>
+              <p>The domain focused on developing applications and systems for the Android operating system, powering a vast majority of mobile devices.</p>
+              <span className="text-lime">03</span>
+            </div>
+          </div>
+
+          <div className="row2">
+            <div className="card bg-light-blue/20">
+              <img src={Design} alt="" />
+              <h2 className="text-light-blue">Design</h2>
+              <p>The creative process of shaping the visual and user interface aspects of digital products, including websites, apps to enhance user engagement and satisfaction.</p>
+              <span className="text-light-blue">04</span>
+            </div>
+            <div className="card bg-[#D2BD0026]">
+              <img src={ML} alt="" />
+              <h2 className="text-[#ECCD2A]">Machine<br />Learning</h2>
+              <p>Harnessing algorithms to enable computers to learn from data, recognize patterns, and make predictions autonomously.</p>
+              <span className="text-[#ECCD2A]">05</span>
+            </div>
+          </div>
+        </div>
+
+        {/* ready? */}
+        <div className="mt-10">
+          <p className="text-6xl w-3/5 mx-auto text-center">Ready to be a part of a thriving community of geeks?</p>
+        </div>
+        {/* timeline */}
+        <div className="flex items-end gap-8">
+          <div className="flex">
+            <img src={Timeline} alt="" />
+            <img className="absolute mt-2" src={Pointer} alt="" />
+          </div>
+          <div className="flex gap-14 text-4xl flex-col pr-8">
+            <div className="flex gap-14 justify-between">
+              <h3>Registrations Start</h3>
+              <h3 className="font-bold date"><span className="text-light-blue">20</span> <span className="text-purple">Feb</span> <span className="text-red">2024</span></h3>
+            </div>
+            <div className="flex gap-14 justify-between">
+              <h3>Projct Submissions</h3>
+              <h3 className="font-bold date"><span className="text-light-blue">27</span> <span className="text-purple">Feb</span> <span className="text-red">2024</span></h3>
+            </div>
+            <div className="flex gap-14 justify-between">
+              <h3>Quiz Round</h3>
+              <h3 className="font-bold date"><span className="text-light-blue">03</span> <span className="text-purple">Mar</span> <span className="text-red">2024</span></h3>
+            </div>
+            <div className="flex gap-14 justify-between">
+              <h3>Interview Shortlisting</h3>
+              <h3 className="font-bold date"><span className="text-light-blue">05</span> <span className="text-purple">Mar</span> <span className="text-red">2024</span></h3>
+            </div>
+            <div className="flex gap-14 justify-between">
+              <h3>Results</h3>
+              <h3 className="font-bold date"><span className="text-light-blue">10</span> <span className="text-purple">Mar</span> <span className="text-red">2024</span></h3>
+            </div>
+          </div>
+        </div>
+
+        {/* easter */}
+        <div className="flex text-xl gap-8 opacity-40 mt-12">
+          <p>------------------------------</p>
+          <p>May the force be with you</p>
+          <p>------------------------------</p>
+        </div>
+
+        {/* admin login */}
+        <a href="/admin-login">
+          <button className="flex gap-4 rounded-full bg-text-box py-1 px-4 hover:bg-grey/5">
+            <p>Admin Login</p>
+            <img src={ArrowRight} alt="" />
+          </button>
+        </a>
       </div>
       <Footer />
     </>
