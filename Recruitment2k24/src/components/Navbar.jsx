2127ecdaa0a99c34288e34ee85017626e1342a25
Aryan Kushwaha <aryankushwaha3101@gmail.com>
2024-01-22 13:24:30 +0530

    minor changes

--- Recruitment2k24/src/components/Navbar.jsx
+++ Recruitment2k24/src/components/Navbar.jsx
@@ -1,7 +1,4 @@
 import logo from "../assets/logo.svg";
-import domainButton from "../assets/domainButton.svg";
-import webButton from "../assets/websiteButton.svg";
-import result from "../assets/result.svg";
 import separator from "../assets/separator.svg";
 import { Link } from "react-router-dom";
 export default function Navbar() {
@@ -23,22 +20,28 @@ export default function Navbar() {
             <img src={separator} alt="separator" className="ml-6 mr-3" />
             <Link
               to="/domains"
-              className="flex items-center gap-2 px-6 rounded-full hover:bg-nav-hover">
-              <img src={`${domainButton}`} alt="Domains" />
-              <div className="text-blue">Domains</div>
+              className="text-blue flex items-center gap-2 px-6 rounded-full hover:bg-nav-hover">
+              <span className="material-symbols-rounded">cards</span>
+              <div>Domains</div>
+            </Link>
+            <Link
+            to="/tasks"
+            className="flex items-center gap-2 px-6 rounded-full hover:bg-nav-hover">
+              <span className="material-symbols-rounded">editor_choice</span>
+              <div>Tasks</div>
             </Link>
             <Link
               to="https://gdscjss.in/"
               target="_blank"
-              className="flex items-center gap-2 px-6 rounded-full hover:bg-nav-hover">
-              <img src={`${webButton}`} alt="Website" />
-              <div className="text-blue">Website</div>
+              className="text-blue flex items-center gap-2 px-6 rounded-full hover:bg-nav-hover">
+              <span className="material-symbols-rounded">language</span>
+              <div>Website</div>
             </Link>
             <Link
               href="/"
-              className="flex items-center gap-2 px-6 rounded-full opacity-60 hover:bg-nav-hover">
-              <img src={`${result}`} alt="result" />
-              <div className="text-blue">Results</div>
+              className="text-blue flex items-center gap-2 px-6 rounded-full opacity-40">
+              <span className="material-symbols-rounded">bar_chart</span>
+              <div>Results</div>
             </Link>
           </div>
           <div>
--- Recruitment2k24/src/pages/user/Homepage.jsx
+++ Recruitment2k24/src/pages/user/Homepage.jsx
@@ -7,7 +7,6 @@ import Homebg from "../../../src/assets/homepagebg.svg";
 import Recruitments from "../../../src/assets/recruitments-text.svg";
 import Timeline from "../../../src/assets/timeine.svg"
 import Pointer from "../../../src/assets/tri.svg"
-import ArrowRight from "../../../src/assets/arrow_circle_right.svg"
 
 import Android from "../../../src/assets/android.svg"
 import WebClub from "../../../src/assets/webclub.svg"
@@ -77,12 +76,14 @@ export default function Homepage() {
 
         <div className="flex items-center gap-12">
           <h2 className="font-bold text-5xl">Register Here</h2>
-          <button className="cta flex items-center gap-6 bg-lime px-10 rounded-xl py-4">
-            <h5 className="text-2xl">Let&apos;s Go</h5>
-            <svg xmlns="http://www.w3.org/2000/svg" width="33" height="24" viewBox="0 0 33 24" fill="none">
-            <path d="M28.9491 13.1501H2.08261C1.75223 13.1501 1.47618 13.0392 1.25448 12.8175C1.03274 12.5958 0.921875 12.3197 0.921875 11.9894C0.921875 11.659 1.03274 11.3829 1.25448 11.1612C1.47618 10.9395 1.75223 10.8286 2.08261 10.8286H28.9491L20.2793 2.15882C20.0531 1.93264 19.9348 1.66627 19.9244 1.35972C19.914 1.05317 20.0323 0.771921 20.2793 0.51598C20.5353 0.259999 20.8114 0.129787 21.1075 0.125337C21.4036 0.120849 21.6797 0.246595 21.9356 0.502576L32.1099 10.6769C32.3123 10.8792 32.4544 11.0861 32.5363 11.2974C32.6181 11.5087 32.659 11.7393 32.659 11.9894C32.659 12.2394 32.6181 12.47 32.5363 12.6813C32.4544 12.8927 32.3123 13.0995 32.1099 13.3019L21.9356 23.4762C21.7094 23.7023 21.4408 23.8207 21.1298 23.8311C20.8188 23.8415 20.5353 23.7187 20.2793 23.4627C20.0323 23.2068 19.9066 22.933 19.9021 22.6413C19.8976 22.3496 20.0234 22.0758 20.2793 21.8199L28.9491 13.1501Z" fill="#2F3B00"/>
-            </svg>
-          </button>
+          <a href="/register">
+            <button className="cta flex items-center gap-6 bg-lime px-10 rounded-xl py-4">
+              <h5 className="text-2xl">Let&apos;s Go</h5>
+              <svg xmlns="http://www.w3.org/2000/svg" width="33" height="24" viewBox="0 0 33 24" fill="none">
+              <path d="M28.9491 13.1501H2.08261C1.75223 13.1501 1.47618 13.0392 1.25448 12.8175C1.03274 12.5958 0.921875 12.3197 0.921875 11.9894C0.921875 11.659 1.03274 11.3829 1.25448 11.1612C1.47618 10.9395 1.75223 10.8286 2.08261 10.8286H28.9491L20.2793 2.15882C20.0531 1.93264 19.9348 1.66627 19.9244 1.35972C19.914 1.05317 20.0323 0.771921 20.2793 0.51598C20.5353 0.259999 20.8114 0.129787 21.1075 0.125337C21.4036 0.120849 21.6797 0.246595 21.9356 0.502576L32.1099 10.6769C32.3123 10.8792 32.4544 11.0861 32.5363 11.2974C32.6181 11.5087 32.659 11.7393 32.659 11.9894C32.659 12.2394 32.6181 12.47 32.5363 12.6813C32.4544 12.8927 32.3123 13.0995 32.1099 13.3019L21.9356 23.4762C21.7094 23.7023 21.4408 23.8207 21.1298 23.8311C20.8188 23.8415 20.5353 23.7187 20.2793 23.4627C20.0323 23.2068 19.9066 22.933 19.9021 22.6413C19.8976 22.3496 20.0234 22.0758 20.2793 21.8199L28.9491 13.1501Z" fill="#2F3B00"/>
+              </svg>
+            </button>
+          </a>
         </div>
         {/* flame */}
         <div className="mt-36 text-center relative">
@@ -114,38 +115,38 @@ export default function Homepage() {
         {/* domains */}
         <div className="cards">
           <div className="row1">
-            <div className="card bg-purple/20">
+            <div className="card bg-purple/20 text-purple font-bold">
               <img src={Programming} alt="" />
-              <h2 className="text-purple">Programming</h2>
+              <h2>Programming</h2>
               <p>The art of instructing computers through code to perform specific tasks, solve problems, and create software applications.</p>
-              <span className="text-purple">01</span>
+              <span>01</span>
             </div>
-            <div className="card bg-red/20">
+            <div className="card bg-red/20 text-red font-bold">
               <img src={WebClub} alt="" />
-              <h2 className="text-red">Web Club</h2>
-              <p> The practice of creating and maintaining websites and web applications, encompassing both front-end and back-end development to ensure a seamless online user experience.</p>
-              <span className="text-red">02</span>
+              <h2>Web Club</h2>
+              <p>Creating wonders in web apps, encompassing both front-end and back-end development to ensure a seamless online user experience.</p>
+              <span>02</span>
             </div>
-            <div className="card bg-lime/20">
+            <div className="card bg-lime/20 text-lime font-bold">
               <img src={Android} alt="" />
-              <h2 className="text-lime">Android</h2>
+              <h2>Android</h2>
               <p>The domain focused on developing applications and systems for the Android operating system, powering a vast majority of mobile devices.</p>
-              <span className="text-lime">03</span>
+              <span>03</span>
             </div>
           </div>
 
           <div className="row2">
-            <div className="card bg-light-blue/20">
+            <div className="card bg-light-blue/20 text-light-blue font-bold">
               <img src={Design} alt="" />
-              <h2 className="text-light-blue">Design</h2>
+              <h2>Design</h2>
               <p>The creative process of shaping the visual and user interface aspects of digital products, including websites, apps to enhance user engagement and satisfaction.</p>
-              <span className="text-light-blue">04</span>
+              <span>04</span>
             </div>
-            <div className="card bg-[#D2BD0026]">
+            <div className="card bg-[#D2BD0026] text-[#ECCD2A] font-bold">
               <img src={ML} alt="" />
-              <h2 className="text-[#ECCD2A]">Machine<br />Learning</h2>
+              <h2>Machine<br />Learning</h2>
               <p>Harnessing algorithms to enable computers to learn from data, recognize patterns, and make predictions autonomously.</p>
-              <span className="text-[#ECCD2A]">05</span>
+              <span>05</span>
             </div>
           </div>
         </div>
@@ -158,7 +159,7 @@ export default function Homepage() {
         <div className="flex items-end gap-8">
           <div className="flex">
             <img src={Timeline} alt="" />
-            <img className="absolute mt-2" src={Pointer} alt="" />
+            <img className="absolute mt-2 -translate-x-2" src={Pointer} alt="" />
           </div>
           <div className="flex gap-14 text-4xl flex-col pr-8">
             <div className="flex gap-14 justify-between">
@@ -195,7 +196,7 @@ export default function Homepage() {
         <a href="/admin-login">
           <button className="flex gap-4 rounded-full bg-text-box py-1 px-4 hover:bg-grey/5">
             <p>Admin Login</p>
-            <img src={ArrowRight} alt="" />
+            <span className="material-symbols-rounded">arrow_circle_right</span>
           </button>
         </a>
       </div>
