5f4d8b85c79931821a42fad74af7f3bf1af51c67
RamitVishwakarma <vishwakarmaramit@gmail.com>
2024-03-29 20:51:57 +0530

    Updated guidelines page

--- Recruitment2k24/src/pages/user/Quizes/Guidelines.jsx
+++ Recruitment2k24/src/pages/user/Quizes/Guidelines.jsx
@@ -7,17 +7,12 @@ export default function Guidelines() {
   const photo = sessionStorage.getItem("photo");
   return (
     <>
-      <div className="h-full">
+      <div className="h-full bg-background">
         <div className="mx-40">
-          <Header>
-            <div className="min-w-80 max-md:mt-10">
-              <HeaderProfile name={name} photo={photo} />
-            </div>
-          </Header>
-          <h1 className="text-5xl font-bold text-center pb-2">
+          <h1 className="text-5xl font-bold mt-[6vh] text-center pb-2">
             Lets start the Quiz!
           </h1>
-          <div className="mx-auto mt-2 w-3/5 h-2/5 flex flex-col justify-center items-center rounded-2xl">
+          <div className="mx-auto bg-text-box shadow-xl  mt-2 w-3/5 h-2/5 flex flex-col justify-center items-center rounded-2xl">
             <div className="text-red font-bold text-[1.7em] text-center p-5">
               Guidelines
             </div>
@@ -41,7 +36,7 @@ export default function Guidelines() {
           </div>
           <Link to="/user/quiz">
             <div className="flex justify-center">
-              <button className="mx-auto my-4 font-bold text-xl text-button-text bg-lime rounded-lg hover:bg-button-hover px-10 py-3">
+              <button className="mx-auto my-6 mb-[6vh] font-bold text-xl text-button-text bg-lime rounded-lg hover:bg-button-hover px-10 py-3">
                 Begin
               </button>
             </div>
