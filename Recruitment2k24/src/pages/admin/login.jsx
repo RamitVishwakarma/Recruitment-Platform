6ad711fb113dff619210d50fe73e5cff210a595d
RamitVishwakarma <vishwakarmaramit@gmail.com>
2024-05-18 04:03:10 +0530

    Fixed admin login page

--- Recruitment2k24/src/pages/admin/login.jsx
+++ Recruitment2k24/src/pages/admin/login.jsx
@@ -39,28 +39,21 @@ const AdminLogin = () => {
 
   return (
     <>
-      <div className="h-screen bg-admin-bg bg-no-repeat bg-left-bottom">
-        <div className="mx-40">
+      <div className="min-h-screen bg-background ">
+        <div className="mx-5 md:mx-20 xl:mx-40 ">
           <Header>
-            <h1 className="text-4xl font-bold text-grey">Admin Login</h1>
+            <h1 className="text-4xl font-bold text-grey max-lg:mt-5">
+              Admin Login
+            </h1>
           </Header>
         </div>
-        <div className="mx-60 mt-48 flex items-center justify-between">
-          {/* side text */}
-          <div>
-            <h1 className="text-8xl font-bold leading-normal text-grey">
-              Welcome
-              <br />
-              Admin
-            </h1>
-          </div>
-
+        <div className=" flex items-center justify-center xl:mt-32 lg:mt-20 md:mt-16 mt-12">
           {/* input fields */}
-          <div className="pr-14 pl-12 py-10 rounded-xl flex flex-col gap-4 items-center relative overflow-hidden bg-text-box">
+          <div className="flex flex-col gap-4 items-center ">
             <h3 className="justify-self-center pl-4 text-4xl font-bold mb-8">
               Welcome&nbsp;Admin
             </h3>
-            <div className="flex z-10 flex-col items-end">
+            <div className="flex z-10 flex-col ">
               {/* Email */}
               <Input
                 id="email"
@@ -73,23 +66,18 @@ const AdminLogin = () => {
               {/* Password */}
               <Input
                 id="password"
-                label="Set Password"
+                label="Password"
                 icon="key"
                 type="password"
                 placeholder="6 characters or more"
                 onChangeHandler={handlePassword}
               />
-              <p className="mt-4 text-light-blue hover:underline">
-                Forgot Password?
-              </p>
               <button
                 onClick={loginHandler}
-                className="mt-10 w-80 font-bold text-2xl text-button-text bg-lime rounded-lg hover:bg-button-hover py-4">
+                className="mt-10 px-24 py-4 max-w-72 mx-auto mr-10 font-bold text-2xl text-button-text bg-lime rounded-lg hover:bg-button-hover ">
                 Log In
               </button>
             </div>
-            <div className="absolute w-96 h-96 -z-1 left-60 top-64 rounded-full blur-2xl bg-red/10"></div>
-            <div className="absolute w-96 h-96 -z-1 right-60 bottom-64 rounded-full blur-2xl bg-red/10"></div>
           </div>
         </div>
       </div>
